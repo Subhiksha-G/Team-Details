ad248f1bfd82654a8dfd5a6c074a5bb55faf1c25
Subhiksha-G <subhikshavkl@gmail.com>
2026-07-08 13:41:23 +0530

    implement item management in todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import CounterUseEffect from "./components/CounterApp/Counter-useEffect";
 import Team from "./components/Team-Directory/Team";
+import Todo from "./components/TodoApp/Todo-useEffect";
 
 function App() {
   return (
     <>
       <Team />
       <CounterUseEffect />
+      <Todo />
     </>
   );
 }
---
+++ src/components/TodoApp/Todo-useEffect.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [tasks, setTasks] = useState("");
+  const [newTask, setNewTask] = useState([]);
+
+  function addTask() {
+    if (tasks) {
+        setNewTask([...newTask, tasks]); 
+        setTasks("");
+    } else {
+        alert("Please enter your task");
+    }
+  }
+
+  function deleteTask(index) {
+    setNewTask(newTask.filter((item, indexValue) => indexValue !== index));
+  }
+  return (
+    <>
+      <h1>Todo App using useEffect</h1>
+      <input
+        type="text"
+        placeholder="Type your task"
+        value={tasks}
+        onChange={(event) => setTasks(event.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {newTask.map((item, index) => (
+            <li key={index}>
+                {item}
+                <button onClick={() => deleteTask(index)}>Delete</button>
+            </li>
+        ))}
+      </ul>
+    </>
+  );
+}
